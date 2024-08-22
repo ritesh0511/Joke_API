d75e15ae24c27bfd5ec4e6b3277dee511d4542c2
Ritesh <diwaneritesh@gmail.com>
2024-08-23 00:34:04 +0530

    Initial commit'

--- Joke_API/asgi.py
+++ Joke_API/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for did_django_chuck_norris_api project.
+ASGI config for Joke_API project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'did_django_chuck_norris_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Joke_API.settings')
 
 application = get_asgi_application()
--- Joke_API/wsgi.py
+++ Joke_API/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for did_django_chuck_norris_api project.
+WSGI config for Joke_API project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'did_django_chuck_norris_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Joke_API.settings')
 
 application = get_wsgi_application()
